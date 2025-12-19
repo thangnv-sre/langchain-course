5664accadfa13897ad30a2cecfe3e93ee4d70f7b
thangnv <thang.nguyenvan@pixta.co.jp>
2025-12-19 15:37:18 +0700

    Implement React agent with output parsing and structured response schema

--- main.py
+++ main.py
@@ -5,25 +5,42 @@ load_dotenv()
 from langchain import hub
 from langchain.agents import AgentExecutor
 from langchain.agents.react.agent import create_react_agent
+from langchain_core.output_parsers.pydantic import PydanticOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
+from prompt import REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS
+from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOpenAI(model="gpt-4")
 react_prompt = hub.pull("hwchase17/react")
+output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
+react_prompt_with_format_instructions = PromptTemplate(
+    template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
+    input_variables=["input", "agent_scratchpad", "tool_names"],
+).partial(format_instructions=output_parser.get_format_instructions())
+
+
 agent = create_react_agent(
     llm=llm,
     tools=tools,
-    prompt=react_prompt,
+    prompt=react_prompt_with_format_instructions,
 )
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-chain = agent_executor
+# chain = agent_executor
+extract_output = RunnableLambda(lambda x: x["output"])
+parse_output = RunnableLambda(lambda x: output_parser.parse(x))
+chain = agent_executor | extract_output |  parse_output
 
 
 def main():
     result = chain.invoke(
-        {"input": "Tìm 2 job devops cho tôi link tuyển dụng ở Việt Nam có mức lương trên 40 triệu đồng/tháng."}
+        input={
+            "input": "search for 3 job devops in hà nội up to 40 triệu",
+        }
     )
     print(result)
 
